fa446be2a5c80a22b0f829b0f946b0fe4dacae12
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-15 08:46:52 -0600

    do some non-default stuff

--- backend/gwd/db/alembic/env.py
+++ backend/gwd/db/alembic/env.py
@@ -57,16 +57,10 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    from gwd.db.database import engine
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+    with engine.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
