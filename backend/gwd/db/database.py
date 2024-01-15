c70db970f456c3442a56b26b89a1f8ce381fd0fe
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-15 17:44:40 -0600

    Add alembic + postgres (#3)
    * init alembic
    
    * do some non-default stuff
    
    * back to default connectable
    
    * add postgres
    
    * connect to postgres via settings
    
    * address mypy errors

--- backend/gwd/db/database.py
+++ backend/gwd/db/database.py
@@ -1,13 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from ..core.config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./gwd.db"
-
-# engine = create_engine(SQLALCHEMY_DATABASE_URL) ## for postgres
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
