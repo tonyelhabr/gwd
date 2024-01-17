c7894d31765d23065db5e7052ced46112fb46072
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-17 11:52:27 -0600

    update start.sh

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-alembic upgrade head && \
-  uvicorn app.main:app --host "0.0.0.0"
+# Let the DB start
+python /app/backend_pre_start.py
+
+# Run migrations
+alembic upgrade head
+
+# Create initial data in DB
+python /app/initial_data.py
+
+uvicorn app.main:app --host "0.0.0.0"
