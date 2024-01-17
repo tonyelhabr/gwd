0003fdd32b23c832b8d6e974f0fbb4d13c076328
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-17 06:54:34 -0600

    add logging to main (altho not using it explicitly)

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,14 @@ from .api import venues, results
 from .db import models
 from .db.database import engine
 
+import logging
+from logging.config import dictConfig
+from .extensions.logger import LogConfig, LOGGER_NAME
+
+dictConfig(LogConfig().model_dump())
+
+logger = logging.getLogger(LOGGER_NAME)
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
