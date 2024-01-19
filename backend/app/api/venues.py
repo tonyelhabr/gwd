5e8bb4cea1f6db932356696f8b86fc9e9d5b58aa
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-18 20:55:49 -0600

    put all endpoints behind api/ update crud

--- backend/app/api/venues.py
+++ backend/app/api/venues.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.crud import venues as v
 from app.db import schemas
 from app.db.database import get_db
-
+from app.services.scraper import ScrapingService
 
 import logging
 from app.extensions.logger import LOGGER_NAME
@@ -15,25 +15,52 @@ logger = logging.getLogger(LOGGER_NAME)
 router = APIRouter()
 
 
-@router.post("/venues/", response_model=schemas.Venue)
+@router.post("/api/venues/", response_model=schemas.Venue)
 def create_venue(venue: schemas.VenueCreate, db: Session = Depends(get_db)):
+    # TODO: Just use get_venue here
     db_venue = v.get_venue_by_name(db, name=venue.name)
     if db_venue:
         raise HTTPException(status_code=400, detail="Venue name already used.")
     return v.create_venue(db=db, venue=venue)
 
 
-@router.get("/venues/", response_model=list[schemas.Venue])
+@router.get("/api/venues/", response_model=list[schemas.Venue])
 def read_venues(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     venues = v.get_venues(db=db, skip=skip, limit=limit)
     return venues
 
 
-@router.get("/venues/{venue_id}", response_model=schemas.Venue)
-def read_venue(venue_id: int, db: Session = Depends(get_db)):
-    db_venue = v.get_venue(db, venue_id=venue_id)
+@router.get("/api/venues/{source_id}", response_model=schemas.Venue)
+def read_venue(source_id: int, db: Session = Depends(get_db)):
+    db_venue = v.get_venue(db, source_id=source_id)
     if db_venue is None:
         raise HTTPException(status_code=404, detail="Venue name not found.")
 
-    logger.info(f"Retrieved venue {db_venue.venue_id}.")
+    logger.info(f"Retrieved venue {db_venue.source_id}.")
     return db_venue
+
+# TODO
+# @router.get("/api/venues/lookup/{name}", response_model=schemas.Venue)
+# def read_venue_by_name(venue_id: int, db: Session = Depends(get_db)):
+#     db_venue = v.get_venue_by_name(db, venue_id=venue_id)
+#     if db_venue is None:
+#         raise HTTPException(status_code=404, detail="Venue name not found.")
+#     logger.info(f"Retrieved venue {db_venue.name}.")
+#     return db_venue
+
+# TODO: Add authentication
+@router.post("/api/scraping/test/venues")
+def test_scraping_venues(db: Session = Depends(get_db)):
+    ss = ScrapingService()
+    logger.info("Starting to scrape venues.")
+    scraped_venues = ss.scrape_venues()
+    logger.info("Finished scraping venues.")
+    for scraped_venue in scraped_venues:
+        existing_venue = v.get_venue(db, source_id=scraped_venue.source_id)
+        if not existing_venue:
+            venue_data = schemas.VenueCreate(**scraped_venue)
+            v.create_venue(db, venue_data)
+        else:
+            venue_data = schemas.VenueUpdate(**scraped_venue)
+            v.update_venue(db, venue_data)
+
--- backend/app/crud/venues.py
+++ backend/app/crud/venues.py
@@ -1,22 +1,26 @@
 from sqlalchemy.orm import Session
-
+from typing import Optional
 from app.db import models, schemas
 from datetime import datetime
 
+import logging
+from app.extensions.logger import LOGGER_NAME
 
-def get_venue(db: Session, venue_id: int):
-    return db.query(models.Venue).filter(models.Venue.id == venue_id).first()
+logger = logging.getLogger(LOGGER_NAME)
+
+def get_venue(db: Session, source_id: str) -> Optional[models.Venue]:
+    return db.query(models.Venue).filter(models.Venue.source_id == source_id).first()
 
 
-def get_venue_by_name(db: Session, name: str):
+def get_venue_by_name(db: Session, name: str) -> Optional[models.Venue]:
     return db.query(models.Venue).filter(models.Venue.name == name).first()
 
 
-def get_venues(db: Session, skip: int = 0, limit: int = 100):
+def get_venues(db: Session, skip: int = 0, limit: int = 100) -> list[models.Venue]:
     return db.query(models.Venue).offset(skip).limit(limit).all()
 
 
-def create_venue(db: Session, venue: schemas.VenueCreate):
+def create_venue(db: Session, venue: schemas.VenueCreate) -> Optional[models.Venue]:
     db_venue = models.Venue(name=venue.name)
     db.add(db_venue)
     db.commit()
@@ -24,22 +28,19 @@ def create_venue(db: Session, venue: schemas.VenueCreate):
     return db_venue
 
 
-def upsert_venue(db: Session, venue: schemas.VenueCreate):
+def update_venue(db: Session, venue: schemas.VenueUpdate) -> Optional[models.Venue]:
     db_venue = (
         db.query(models.Venue).filter(models.Venue.source_id == venue.source_id).first()
     )
-    if db_venue:
-        # Update existing venue
-        for key, value in venue.dict().items():
-            setattr(db_venue, key, value)
-        # TODO: probably ok to remove this since we update it with `onupdate` in the model.
-        db_venue.updated_at = (
-            datetime.utcnow()
-        )  # explicitly update the 'updated_at' field
-    else:
-        # Create new venue
-        db_venue = models.Venue(**venue.dict())
-        db.add(db_venue)
+    if not db_venue:
+        return None
+    
+    # Update existing venue
+    for key, value in venue.dict().items():
+        setattr(db_venue, key, value) if value else None
+        
+    # TODO: probably ok to remove this since we update it with `onupdate` in the model.
+    db_venue.updated_at = datetime.utcnow()
 
     db.commit()
     db.refresh(db_venue)
