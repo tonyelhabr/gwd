cde34311c5c93a42fa361ddf7230e17515df7e2d
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-19 15:23:03 -0600

    Fix scraping service (#20)
    * lots more logging to try to figure out wtf is going on
    
    * f string
    
    * trying everything possible to get scraper to work... looks like waiting before parsing the page is the culprit
    
    * looks like time.sleep(3) is going to have to be the solution for now since i can't get implicit or explicit waits to work
    
    * don't need to log the dictionary

--- backend/app/api/venues.py
+++ backend/app/api/venues.py
@@ -14,12 +14,16 @@ logger = logging.getLogger(LOGGER_NAME)
 
 router = APIRouter()
 
+
 # TODO: Add authentication
 @router.post("/api/venues/", response_model=schemas.Venue)
 def create_venue(venue: schemas.VenueCreate, db: Session = Depends(get_db)):
     db_venue = v.get_venue(db, source_id=venue.source_id)
     if db_venue:
-        raise HTTPException(status_code=400, detail=f"Venue source ID '{venue.source_id}' already used.")
+        raise HTTPException(
+            status_code=400,
+            detail=f"Venue source ID '{venue.source_id}' already used.",
+        )
     return v.create_venue(db=db, venue=venue)
 
 
@@ -34,9 +38,11 @@ def read_venues(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 def read_venue(source_id: str, db: Session = Depends(get_db)):
     db_venue = v.get_venue(db, source_id=source_id)
     if db_venue is None:
-        raise HTTPException(status_code=404, detail=f"Venue source ID '{source_id}' not found.")
+        raise HTTPException(
+            status_code=404, detail=f"Venue source ID '{source_id}' not found."
+        )
 
-    logger.info(f"Retrieved venue {db_venue.source_id}.")
+    logger.info(f"Retrieved venue {db_venue['source_id']}.")
     return db_venue
 
 
@@ -46,8 +52,11 @@ def read_venue_by_name(name: str, db: Session = Depends(get_db)):
     db_venue = v.get_venue_by_name(db, name=name)
     if db_venue is None:
         raise HTTPException(status_code=404, detail=f"Venue name '{name}' not found.")
-    logger.info(f"Retrieved the venue with name '{db_venue.name}' (source ID: '{db_venue.source_id}').")
+    logger.info(
+        f"Retrieved the venue with name '{db_venue['name']}' (source ID: '{db_venue['source_id']}')."
+    )
     return db_venue
+
 
 # TODO: Add authentication
 @router.post("/api/scraping/test/venues")
@@ -56,11 +65,18 @@ def test_scraping_venues(db: Session = Depends(get_db)):
     logger.info("Starting to scrape venues.")
     scraped_venues = ss.scrape_venues()
     logger.info("Finished scraping venues.")
+    logger.info(f"Found {len(scraped_venues)} venues.")
     for scraped_venue in scraped_venues:
-        existing_venue = v.get_venue(db, source_id=scraped_venue.source_id)
+        existing_venue = v.get_venue(db, source_id=scraped_venue["source_id"])
         if not existing_venue:
+            logger.info(
+                f"Venue {scraped_venue['source_id']} does not exist in the DB, so creating it."
+            )
             venue_to_create = schemas.VenueCreate(**scraped_venue)
             v.create_venue(db, venue_to_create)
         else:
+            logger.info(
+                f"Venue {existing_venue.source_id} exists in the DB, so updating it."
+            )
             venue_to_update = schemas.VenueUpdate(**scraped_venue)
             v.update_venue(db, venue_to_update)
--- backend/app/services/scraper.py
+++ backend/app/services/scraper.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from urllib.parse import urlencode, urlunparse
 import logging
 from app.extensions.logger import LOGGER_NAME
+import time
 
 logger = logging.getLogger(LOGGER_NAME)
 
@@ -14,6 +15,7 @@ logger = logging.getLogger(LOGGER_NAME)
 class ScrapingService:
     def __init__(self):
         self.base_url = "www.geekswhodrink.com"
+        logger.info("Creating driver")
         self.driver = self.create_driver()
 
     def _set_chrome_options(self):
@@ -21,14 +23,16 @@ class ScrapingService:
         options = [
             "--headless",
             "--no-sandbox",
-            "--disable-dev-shm-usage"
+            "--disable-dev-shm-usage",
         ]
         for option in options:
             chrome_options.add_argument(option)
+
+        logger.info("Setting options")
         return chrome_options
 
     def create_driver(self):
-        return webdriver.Chrome(options=self._set_chrome_options())
+        return webdriver.Chrome(self._set_chrome_options())
 
     def _create_venues_url(self):
         path = "venues"
@@ -46,7 +50,9 @@ class ScrapingService:
                     )
                 )
             )
+            logger.info("Popup button found")
             popup_button.click()
+            logger.info("Popup button clicked.")
         except NoSuchElementException:
             logger.error("Popup button not found. Exiting early.")
             self.driver.quit()
@@ -54,7 +60,8 @@ class ScrapingService:
 
     def _extract_venues_data(self):
         logger.info("Waiting for all venues to load on the source page.")
-        WebDriverWait(self.driver, 3).until(lambda driver: True)
+        # WebDriverWait(self.driver, 10).until(lambda driver: True)
+        time.sleep(3)
         soup = BeautifulSoup(self.driver.page_source, "html.parser")
         logger.info("All venues have loaded on the source page.")
         results = soup.find_all("div", class_="find__col find__col--list")
@@ -75,7 +82,11 @@ class ScrapingService:
         return data
 
     def scrape_venues(self):
-        self.driver.get(self._create_venues_url())
-        self._handle_venues_popup()
-        self._extract_venues_data()
-        self.driver.quit()
+        try:
+            url = self._create_venues_url()
+            self.driver.get(url)
+            self._handle_venues_popup()
+            scraped_data = self._extract_venues_data()
+            return scraped_data
+        finally:
+            self.driver.quit()
