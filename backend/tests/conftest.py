025f60583523f2fa2cd5e17934195f2b762e1e62
Tony ElHabr <anthonyelhabr@gmail.com>
2024-01-23 07:20:06 -0600

    tests run fine

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -40,9 +40,3 @@ def test_venue_data():
         "address": "123 New St",
         "url": "http://newvenue.com"
     }
-
-def test_create_venue_success(client, test_venue_data):
-    response = client.post("/api/venues/", json=test_venue_data)
-
-    assert response.status_code == 201
-    assert response.json()["source_id"] == test_venue_data["source_id"]
